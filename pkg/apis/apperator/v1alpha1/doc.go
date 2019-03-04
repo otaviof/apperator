4cff459e93fa38fe62a8b990850766275b006d43
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-04 15:12:32 +0100

    ApperatorApp API definition.
    $ operator-sdk add api --api-version apperator.otaviof.github.io/v1alpha1 --kind ApperatorApp

---
+++ pkg/apis/apperator/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the apperator v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=apperator.otaviof.github.io
+package v1alpha1
