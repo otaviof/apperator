baffad4225afbbfcf7f53bf8b3abd6fd66fc870b
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-08 18:37:56 +0100

    Rethinking types.

--- pkg/apis/apperator/v1alpha1/apperatorapp_types.go
+++ pkg/apis/apperator/v1alpha1/apperatorapp_types.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -12,8 +14,39 @@ import (
 // ApperatorAppSpec defines the desired state of ApperatorApp
 // +k8s:openapi-gen=true
 type ApperatorAppSpec struct {
-	Deployment DeploymentSpec `json:"deployment"`
+	Deployment     DeploymentSpec  `json:"deployment"`
+	Envs           []EnvSpec       `json:"envs"`
+	InitContainers []ContainerSpec `json:"initContainers"`
+	Sidecars       []ContainerSpec `json:"sidecars"`
+	Probes         []ProbeSpec     `json:"probes"`
+	Vault          []VaultSpec     `json:"vault"`
 }
+
+// DeploymentSpec describes a core-v1 deployment spec object
+// +k8s:openapi-gen=true
+type DeploymentSpec struct {
+	Spec appsv1.DeploymentSpec `json:"spec"`
+}
+
+// EnvSpec wrapper for EnvVar
+// +k8s:openapi-gen=true
+type EnvSpec struct {
+	Env []corev1.EnvVar `json:"env"`
+}
+
+// ContainerSpec wrapper for Container
+// +k8s:openapi-gen=true
+type ContainerSpec struct {
+	Spec corev1.Container `json"spec"`
+}
+
+// ProbeSpec describes a Prometheus probe
+// +k8s:openapi-gen=true
+type ProbeSpec struct{}
+
+// VaultSpec describes a Vault ready init-container
+// +k8s:openapi-gen=true
+type VaultSpec struct{}
 
 // ApperatorAppStatus holds the operator status
 // +k8s:openapi-gen=true
