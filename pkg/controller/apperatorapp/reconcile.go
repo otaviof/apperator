e4ce8c66ebbae7e239bbbbe73955a98210c80ad0
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-18 17:25:54 +0100

    Writting reconciliation logic.

--- pkg/controller/apperatorapp/reconcile.go
+++ pkg/controller/apperatorapp/reconcile.go
@@ -2,8 +2,7 @@ package apperatorapp
 
 import (
 	"context"
-
-	"k8s.io/apimachinery/pkg/types"
+	"reflect"
 
 	logr "github.com/go-logr/logr"
 	v1alpha1 "github.com/otaviof/apperator/pkg/apis/apperator/v1alpha1"
@@ -32,60 +31,56 @@ func (r *ReconcileApperatorApp) Reconcile(req reconcile.Request) (reconcile.Resu
 
 	app := &v1alpha1.ApperatorApp{}
 	if err = r.client.Get(context.TODO(), req.NamespacedName, app); err != nil {
-		if errors.IsNotFound(err) {
-			return reconcile.Result{}, nil
+		if !errors.IsNotFound(err) {
+			return reconcile.Result{}, err
 		}
+		return reconcile.Result{}, nil
+	}
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+		},
+	}
+
+	controller := NewDeployment(r.client, app)
+	if deployment.Spec, err = controller.RenderSpec(); err != nil {
+		reqLogger.Error(err, "Failed to render a new deployment")
 		return reconcile.Result{}, err
 	}
 
-	deployment := &appsv1.Deployment{}
-	if err = r.client.Get(context.TODO(), req.NamespacedName, deployment); err != nil {
-		if errors.IsNotFound(err) {
-			deployment = r.createDeployment(app)
-		} else {
-			reqLogger.Error(err, "Failed to read deployment '%s'", app.ObjectMeta.Name)
+	reqLogger.Info("Looking for deployment object")
+	currentDeployment := &appsv1.Deployment{}
+	if err = r.client.Get(context.TODO(), req.NamespacedName, currentDeployment); err != nil {
+		if !errors.IsNotFound(err) {
+			reqLogger.Error(err, "Failed to read deployment")
 		}
+
+		reqLogger.Info("Creating a new deployment", "name", deployment.Name)
+		if err = r.client.Create(context.TODO(), deployment); err != nil {
+			reqLogger.Error(err, "Failed to create deployment")
+			return reconcile.Result{}, err
+		}
+
+		return reconcile.Result{Requeue: true}, nil
 	}
 
+	reqLogger.Info("Checking if deployment is up-to-date", "name", currentDeployment.Name)
+	if !reflect.DeepEqual(currentDeployment.Spec, deployment.Spec) {
+		reqLogger.Info("Updating deployment", "name", currentDeployment.Name)
+		currentDeployment.Spec = deployment.Spec
+
+		if err = r.client.Update(context.TODO(), currentDeployment); err != nil {
+			reqLogger.Error(err, "Failed to update deployment")
+			return reconcile.Result{}, err
+		}
+
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	reqLogger.Info("Deployment is up-to-date", "name", currentDeployment.Name)
 	return reconcile.Result{}, nil
-}
-
-// createDeployment puts together a deployment based in the informed instance details.
-func (r *ReconcileApperatorApp) createDeployment(app *v1alpha1.ApperatorApp) *appsv1.Deployment {
-	return &appsv1.Deployment{
-		TypeMeta:   metav1.TypeMeta{APIVersion: "apps/v1", Kind: "Deployment"},
-		ObjectMeta: metav1.ObjectMeta{Name: app.Name, Namespace: app.Namespace},
-		Spec:       app.Spec.Deployment.Spec,
-	}
-}
-
-// mergeEnvs execute the merge of upstream ApperatorEnv objects into current deployment.
-func (r *ReconcileApperatorApp) mergeEnvs(envNames []string, deployment *appsv1.Deployment) error {
-	var err error
-
-	log.Info("Merging ApperatorEnv objects ('%#v') into deployment.", envNames)
-	if len(deployment.Spec.Template.Spec.Containers) == 0 {
-		log.Info("Warning! Deployment '%s' does not define any containers!", deployment.ObjectMeta.Name)
-		return nil
-	}
-
-	for _, envName := range envNames {
-		env := &v1alpha1.ApperatorEnv{}
-		meta := types.NamespacedName{Name: envName, Namespace: deployment.ObjectMeta.Namespace}
-
-		if err = r.client.Get(context.TODO(), meta, env); err != nil {
-			return err
-		}
-
-		for _, envVar := range env.Spec.Env {
-			deployment.Spec.Template.Spec.Containers[0].Env = append(
-				deployment.Spec.Template.Spec.Containers[0].Env,
-				envVar,
-			)
-		}
-	}
-
-	return nil
 }
 
 // newReconcileApperatorApp creates a new instance setting up logger first.
